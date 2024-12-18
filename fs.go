51a7d4c935223f062dce1faebcdefac42092b86c
hollowdll <pixerchat@gmail.com>
2024-12-18 21:25:54 +0200

    add file write/read

---
+++ fs.go
@@ -0,0 +1,19 @@
+package hakjpass
+
+import "os"
+
+func writeToFile(filepath string, data []byte) error {
+	err := os.WriteFile(filepath, data, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func readFile(filepath string) ([]byte, error) {
+	data, err := os.ReadFile(filepath)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
