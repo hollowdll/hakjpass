fb40962a9f8b7439008ae953fbda1f66b570eff3
hollowdll <pixerchat@gmail.com>
2024-12-20 20:59:40 +0200

    fs additions

--- fs.go
+++ fs.go
@@ -2,10 +2,18 @@ package hakjpass
 
 import (
 	"os"
+	"path/filepath"
 )
 
-func writeToFile(filepath string, data []byte) error {
-	err := os.WriteFile(filepath, data, 0644)
+const (
+	PasswordStorageFileName       string = "hakjpass_storage"
+	HakjpassDataDirName           string = "hakjpass-data"
+	dataDirPermission                    = 0700
+	PasswordStorageFilePermission        = 0600
+)
+
+func writeToFile(filepath string, data []byte, perm os.FileMode) error {
+	err := os.WriteFile(filepath, data, perm)
 	if err != nil {
 		return err
 	}
@@ -18,4 +26,37 @@ func readFile(filepath string) ([]byte, error) {
 		return nil, err
 	}
 	return data, nil
+}
+
+func createDataDirIfNotExists() error {
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		return err
+	}
+
+	dataDir := filepath.Join(configDir, HakjpassDataDirName)
+
+	err = os.MkdirAll(dataDir, dataDirPermission)
+	if err != nil {
+		return err
+	}
+
+	err = os.Chmod(dataDir, dataDirPermission)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func fileExists(filepath string) (bool, error) {
+	_, err := os.Stat(filepath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false, nil
+		} else {
+			return false, err
+		}
+	}
+	return true, nil
 }
